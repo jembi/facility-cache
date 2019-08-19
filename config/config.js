59d0212c69217e0255d105823aabe5f1426345ce
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2019-08-19 16:55:51 +0200

    Allow environment vars to override file config MOM-555

---
+++ config/config.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function configure () {
+  return Object.freeze({
+    "server": Object.freeze({
+      "hostname": process.env.SERVER_HOSTNAME || "localhost",
+      "port": process.env.SERVER_PORT || 8001
+    }),
+    
+    "logger": Object.freeze({
+      "level": "info"
+    }),
+    
+    "statsd": Object.freeze({
+      "host": process.env.STATSD_HOST || "localhost",
+      "port": process.env.STATSD_PORT || 8125
+    }),
+    
+    "openhim": Object.freeze({
+      "api" : {
+        "username": process.env.OPENHIM_API_USERNAME || "root@openhim.org",
+        "password": process.env.OPENHIM_API_PASSWORD || "openhim-password",
+        "apiURL": process.env.OPENHIM_API_APIURL || "https://localhost:8080",
+        "trustSelfSigned": process.env.TRUSTED_SELF_SIGNED || true
+      },
+      "register": true,
+      "heartbeat": true
+    })
+  })
+}
+
+exports.configure = configure
