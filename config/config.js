611e5a303be9e482f59f69853a08b6faf9464e98
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2019-08-20 09:49:07 +0200

    Change environment variable names MOM-555

--- config/config.js
+++ config/config.js
@@ -6,21 +6,21 @@ function configure () {
       "hostname": process.env.SERVER_HOSTNAME || "localhost",
       "port": process.env.SERVER_PORT || 8001
     }),
-    
+
     "logger": Object.freeze({
       "level": "info"
     }),
-    
+
     "statsd": Object.freeze({
       "host": process.env.STATSD_HOST || "localhost",
       "port": process.env.STATSD_PORT || 8125
     }),
-    
+
     "openhim": Object.freeze({
       "api" : {
-        "username": process.env.OPENHIM_API_USERNAME || "root@openhim.org",
-        "password": process.env.OPENHIM_API_PASSWORD || "openhim-password",
-        "apiURL": process.env.OPENHIM_API_APIURL || "https://localhost:8080",
+        "username": process.env.OPENHIM_USERNAME || "root@openhim.org",
+        "password": process.env.OPENHIM_PASSWORD || "openhim-password",
+        "apiURL": process.env.OPENHIM_URL || "https://localhost:8080",
         "trustSelfSigned": process.env.TRUSTED_SELF_SIGNED || true
       },
       "register": true,
